e5a43c51ad86a33e01c250ec582c265a8880ec86
Your Name <kaiwen.su@alumni.ubc.ca>
2022-04-11 15:33:23 +0000

    Accordian Style

--- app.js
+++ app.js
@@ -1,35 +1,7 @@
-const datas = [{
-    title: "Can you accept all credit cards",
-    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ipsa? Nam nobis repudiandae ullam,",
-},
-{
-    title: "Can you accept all credit cards",
-    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ipsa? Nam nobis repudiandae ullam,",
-},
-{
-    title: "Can you accept all credit cards",
-    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ipsa? Nam nobis repudiandae ullam,",
+const accordion = document.getElementsByClassName('container');
+
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
 }
-];
-
-
-let container = document.querySelector(".container");
-let accordion = document.querySelector(".accordion");
-let accordion__content = document.querySelectorAll(".accordion__content");
-let accordion__show = document.querySelectorAll(".accordion__show");
-
-
-accordion__show.forEach(function (current, index) {
-current.addEventListener("click", (e) => {
-    e.preventDefault();
-    accordion__content[index].classList.toggle("show_text");
-    accordion__content[index].innerHTML = datas[0].content;
-
-    if (current.innerHTML === "+") {
-        current.innerHTML = "-";
-    } else {
-        current.innerHTML = "+";
-    }
-
-})
-})
